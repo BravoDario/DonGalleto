e29895a6dfcf6630e541cc3d77b5f2c4576f9192
chekho <sergiocecyteg@gmail.com>
2023-12-03 15:35:04 -0600

    modificado modelo detalle venta

--- Don Galleto/dg_model/src/com/eddit/dg/model/DetalleVenta.java
+++ Don Galleto/dg_model/src/com/eddit/dg/model/DetalleVenta.java
@@ -8,51 +8,23 @@ import java.util.List;
  */
 public class DetalleVenta {
 
-    private int idDetalle_venta;
-    private int cantidad;
-    private int tipo_venta;
-    private List<Galleta> galletas;
+    private Venta venta;
+    private List<VentaGalleta> vg;
 
-    public DetalleVenta() {
+    public Venta getVenta() {
+        return venta;
     }
 
-    public DetalleVenta(int idDetalle_venta, int cantidad, int tipo_venta, List<Galleta> galletas) {
-        this.idDetalle_venta = idDetalle_venta;
-        this.cantidad = cantidad;
-        this.tipo_venta = tipo_venta;
-        this.galletas = galletas;
+    public void setVenta(Venta venta) {
+        this.venta = venta;
     }
 
-    public List<Galleta> getGalletas() {
-        return galletas;
+    public List<VentaGalleta> getVg() {
+        return vg;
     }
 
-    public void setGalletas(List<Galleta> galletas) {
-        this.galletas = galletas;
+    public void setVg(List<VentaGalleta> vg) {
+        this.vg = vg;
     }
-
-    public int getIdDetalle_venta() {
-        return idDetalle_venta;
-    }
-
-    public void setIdDetalle_venta(int idDetalle_venta) {
-        this.idDetalle_venta = idDetalle_venta;
-    }
-
-    public int getCantidad() {
-        return cantidad;
-    }
-
-    public void setCantidad(int cantidad) {
-        this.cantidad = cantidad;
-    }
-
-    public int getTipo_venta() {
-        return tipo_venta;
-    }
-
-    public void setTipo_venta(int tipo_venta) {
-        this.tipo_venta = tipo_venta;
-    }
-
+    
 }
