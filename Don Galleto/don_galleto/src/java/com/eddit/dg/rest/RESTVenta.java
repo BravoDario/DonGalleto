7726bc472a79a42d128feacb21c82d61a1d39c9c
chekho <sergiocecyteg@gmail.com>
2023-12-03 15:35:55 -0600

    agregado rest venta

--- Don Galleto/don_galleto/src/java/com/eddit/dg/rest/RESTVenta.java
+++ Don Galleto/don_galleto/src/java/com/eddit/dg/rest/RESTVenta.java
@@ -1,10 +1,51 @@
-
 package com.eddit.dg.rest;
+
+import com.eddit.dg.core.ControllerVenta;
+import com.eddit.dg.model.DetalleVenta;
+import com.eddit.dg.model.Venta;
+import com.google.gson.Gson;
+import com.google.gson.JsonParseException;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.*;
+import java.sql.SQLException;
 
 /**
  *
  * @author Angel
  */
+@Path("venta")
 public class RESTVenta {
-    
+
+    @Path("save")
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response save(@FormParam("datosVenta") @DefaultValue("") String datos) {
+        String out = null;
+        Gson gson = new Gson();
+        DetalleVenta d = null;
+        ControllerVenta cv = new ControllerVenta();
+
+        try {
+
+            d = gson.fromJson(datos, DetalleVenta.class);
+
+            cv.generarVenta(d);
+
+            out = gson.toJson(d);
+
+        } catch (JsonParseException jpe) {
+
+            jpe.printStackTrace();
+            out = "{\"exeption\":\"Formato JSON de Datos Incorrecto\"}";
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            out = "{\"exeption\":\"%s\"}";
+            
+            out = String.format(out, e.toString());
+        }
+        return Response.status(Response.Status.OK).entity(out).build();
+    }
+
 }
