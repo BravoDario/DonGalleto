caa4885455abc5c7cbece8e6be7a1d7cb8ddb0f3
chekho <sergiocecyteg@gmail.com>
2023-12-02 20:49:50 -0600

    Modificado funcion de seleccion de productos (no back)

--- Don Galleto/don_galleto/web/admin/js/venta.js
+++ Don Galleto/don_galleto/web/admin/js/venta.js
@@ -15,10 +15,30 @@ export function selectImage(imageElement, panelId) {
     selectedImages[panelId] = imageElement;
 
     if (selectedImages['panel1'] && selectedImages['panel2']) {
-        alert('Ambas imágenes están seleccionadas');
-        selectedImages['panel1'].classList.remove('selected');
-        selectedImages['panel2'].classList.remove('selected');
-        selectedImages['panel1'] = null;
-        selectedImages['panel2'] = null;
+        showModal('Las imágenes seleccionadas son ' + selectedImages['panel1'].dataset.name + ' y ' + selectedImages['panel2'].dataset.name);
     }
+}
+
+function showModal(text) {
+    document.getElementById('modalText').textContent = text;
+    document.getElementById('myModal').style.display = "block";
+}
+
+export function closeModal() {
+    document.getElementById('myModal').style.display = "none";
+    selectedImages['panel1'].classList.remove('selected');
+    selectedImages['panel2'].classList.remove('selected');
+    selectedImages['panel1'] = null;
+    selectedImages['panel2'] = null;
+}
+
+export function addToTable() {
+    var table = document.getElementById('imageTable');
+    var row = table.insertRow();
+    var cell1 = row.insertCell();
+    var cell2 = row.insertCell();
+    cell1.textContent = selectedImages['panel1'].dataset.name;
+    cell2.textContent = selectedImages['panel2'].dataset.name;
+    closeModal();
+
 }
