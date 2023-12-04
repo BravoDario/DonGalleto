61877b98dfc0510d58e57afe9047e055663d4292
chekho <sergiocecyteg@gmail.com>
2023-12-04 13:46:48 -0600

    agregado funcion de identificar galleta (sin db)

--- Don Galleto/don_galleto/web/admin/js/venta.js
+++ Don Galleto/don_galleto/web/admin/js/venta.js
@@ -1,5 +1,9 @@
-export function inicializarVenta() {
+var datosDeGalletas;
 
+export function inicializarVenta() {
+    fetch("../don_galleto/admin/json/galletas.json")
+            .then(response => response.json())
+            .then(data => datosDeGalletas = data);
 }
 
 var selectedImages = {
@@ -15,9 +19,36 @@ export function selectImage(imageElement, panelId) {
     selectedImages[panelId] = imageElement;
 
     if (selectedImages['panel1'] && selectedImages['panel2']) {
+        let val1 = selectedImages['panel1'].dataset.name;
+        let val2 = selectedImages['panel2'].dataset.name;
+        let galleta = datosDeGalletas.galletas.find(galleta => galleta.id === val1);
+        console.log(galleta.precio);
+        let et = document.getElementById("txtmet");
+        
+        switch (val2) {
+            case "A":
+                et.textContent = "pesos";
+                break;
+            case "B":
+                et.textContent = "gr";
+                break;
+            case "C":
+                et.textContent = "caja";
+                break;
+            case "D":
+                et.textContent = "pieza";
+                break;
+                // puedes agregar más casos aquí...
+            default:
+                break;
+        }
+
+
         showModal('Las imágenes seleccionadas son ' + selectedImages['panel1'].dataset.name + ' y ' + selectedImages['panel2'].dataset.name);
     }
 }
+
+
 
 function showModal(text) {
     document.getElementById('modalText').textContent = text;
@@ -37,6 +68,7 @@ export function addToTable() {
     var row = table.insertRow();
     var cell1 = row.insertCell();
     var cell2 = row.insertCell();
+    var cell3 = row.insertCell();
     cell1.textContent = selectedImages['panel1'].dataset.name;
     cell2.textContent = selectedImages['panel2'].dataset.name;
     closeModal();
