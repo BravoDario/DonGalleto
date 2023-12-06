ca94bfa9d681ea655d76fbb496807c9124735d9b
chekho <sergiocecyteg@gmail.com>
2023-12-06 11:01:52 -0600

    agragdo modelo de reporteTabla(para graficas)

---
+++ Don Galleto/dg_model/src/com/eddit/dg/model/reporteTabla.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.eddit.dg.model;
+
+/**
+ *
+ * @author sergi
+ */
+public class reporteTabla {
+    private int cantidad;
+    private String fecha_venta;
+    private double ingresos;
+    private double gastos;
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getFecha_venta() {
+        return fecha_venta;
+    }
+
+    public void setFecha_venta(String fecha_venta) {
+        this.fecha_venta = fecha_venta;
+    }
+
+    public double getIngresos() {
+        return ingresos;
+    }
+
+    public void setIngresos(double ingresos) {
+        this.ingresos = ingresos;
+    }
+
+    public double getGastos() {
+        return gastos;
+    }
+
+    public void setGastos(double gastos) {
+        this.gastos = gastos;
+    }
+    
+    
+}
